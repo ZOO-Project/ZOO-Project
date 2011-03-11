260a203452316b82d4d6e942feba0a1ed5be183c
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2011-03-11 22:03:57 +0000

    latest sphinx hacks
    git-svn-id: http://svn.zoo-project.org/svn/trunk@120 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- docs/conf.py
+++ docs/conf.py
@@ -96,35 +96,37 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {
-#     "rightsidebar": "true"
-#    "headbgcolor": "#ffffff",
-#    "bgcolor": "#ffffff",
-#    "textcolor": "#646464",
-#    "linkcolor": "#fc9326",
-#    "visitedlinkcolor": "#fc9326",    
-#    "sidebarbgcolor": "red",
-#    "relbarbgcolor": "#5e3905"    
-#}
+html_theme_options = {
+    "rightsidebar": "true",
+    "headbgcolor": "#ffffff",
+    "bgcolor": "#ffffff",
+    "textcolor": "#333333",
+    "linkcolor": "#fc9326",
+    "visitedlinkcolor": "#fc9326",    
+    "sidebarbgcolor": "white",
+    "footerbgcolor": "#ffffff",
+    "footertextcolor": "#646464",    
+    "relbarbgcolor": "#5e3905"    
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "ZOO Project " + release + " documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = "Documentation"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+#html_logo = 
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -140,7 +142,9 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+'**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+"index":["indexsidebar.html",'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
@@ -156,7 +160,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
