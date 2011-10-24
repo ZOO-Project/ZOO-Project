4d6202e4f274c75489941bdfc8262ccbc85bd8a7
Luca Delucchi <lucadeluge@gmail.com>
2011-10-24 13:42:33 +0000

    change documentation and example of Javascript code because they did not work
    git-svn-id: http://svn.zoo-project.org/svn/trunk@343 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- zoo-project/zoo-services/hello-js/cgi-env/hello.js
+++ zoo-project/zoo-services/hello-js/cgi-env/hello.js
@@ -1,12 +1,12 @@
 
 function hellojs(conf,inputs,outputs){
-	outputs[0]["result"]["value"]="Hello "+inputs[0]["S"]["value"]+" from the JS World !";
+	outputs["result"]["value"]="Hello "+inputs["S"]["value"]+" from the JS World !";
 	//SERVICE_SUCEEDED
 	return Array(3,outputs);
 }
 
 function hellojs1(conf,inputs,outputs){
-	outputs[0]["result"]["value"]="Hello "+inputs[0]["S"]["value"]+" from the JS World !";
+	outputs["result"]["value"]="Hello "+inputs["S"]["value"]+" from the JS World !";
 	//SERVICE_SUCEEDED
 	return {"result":3,"outputs": outputs};
 }
