0ddbfb05303623be3ccb5520f861e260b0d264c6
Gérald Fenoy <gerald.fenoy@geolabs.fr>
2012-09-24 06:40:22 +0000

    Remove uneeded header file and small fix to compile on win32 and use correct way to fill outputs parameters.
    git-svn-id: http://svn.zoo-project.org/svn/trunk@363 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- zoo-project/zoo-services/arithmetics/test_service.c
+++ zoo-project/zoo-services/arithmetics/test_service.c
@@ -33,71 +33,10 @@ __declspec(dllexport)
 		fprintf(stderr,"\nService internal print (B value: %i)\n",B);
 	}
 	res=A*B;
-	outputs=(maps*)malloc(sizeof(maps*));
-	outputs->name="Result";
 	char tmp[256];
 	sprintf(tmp,"%i",res);
-	outputs->content=createMap("value",tmp);
-	addMapToMap(&outputs->content,createMap("datatype","float"));
-	addMapToMap(&outputs->content,createMap("uom","meter"));
-	outputs->next=NULL;
-	dumpMaps(outputs);
-  	fprintf(stderr,"\nService internal print\n===\n");
+	setMapInMaps(outputs,"Result","value",tmp);
 	return SERVICE_SUCCEEDED;
-  }
-
-  int helloworld1(maps*& conf,maps*& inputs,maps*& outputs){
-    outputs=(maps*)malloc(sizeof(maps*));
-    outputs->name="Result";
-    outputs->content=createMap("value","Hello World");
-    addMapToMap(&outputs->content,createMap("datatype","string"));
-    return SERVICE_SUCCEEDED; 
-  }
-
-  int helloworld(map*& inputs,map*& outputs){
-    outputs=createMap("output_0","Hello World\n");
-    return SERVICE_SUCCEEDED; 
-  }
-
-  int printArguments(map*& inputs,map*& outputs){
-    char *res=(char *)malloc(sizeof(char));
-    map* tmp=inputs;
-    while(tmp!=NULL){
-      res=(char *)realloc(res,strlen(res)+strlen(tmp->value)+strlen(tmp->name)+6);
-      sprintf(res,"%s,\"%s\"=\"%s\"",res,tmp->name,tmp->value);
-      //sprintf(res,"%s,\"%s\"=\"%s\"",res,tmp->name,tmp->value);
-      tmp=tmp->next;
-    }
-    char *tmpVal=strdup(res+1);
-    outputs=createMap("output_0",tmpVal);
-    addToMap(outputs,"output_1",tmpVal);
-
-    /*dumpMap(outputs);
-      dumpMap(inputs);*/
-    return SERVICE_SUCCEEDED; 
-  }
-
-  int buildJsonArrayOfArgs(map*& inputs,map*& outputs){
-    char *res=(char *)malloc(sizeof(char));
-    map* tmp=inputs;
-    while(tmp!=NULL){
-      res=(char *)realloc(res,strlen(res)+strlen(tmp->value)+3);
-      sprintf(res,"%s,\"%s\"",res,tmp->value);
-      tmp=tmp->next;
-    }
-    char *tmpVal;
-    if(strncmp(res,",",1)!=0){
-      free(tmpVal);
-      tmpVal=strdup(res+1);
-      //dumpMap(inputs);      
-    }else
-      tmpVal=strdup(res);
-    tmpVal=(char*)realloc(tmpVal,strlen(tmpVal)+2);
-    sprintf(tmpVal,"[%s]",tmpVal);
-    outputs=createMap("output_0",tmpVal+1);
-    //dumpMap(outputs);
-    //dumpMap(inputs);
-    return SERVICE_SUCCEEDED; 
   }
 
 }
