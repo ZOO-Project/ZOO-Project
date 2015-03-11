b13cf84f616b3a5d67c77c6dfa7b4aeab261d673
David Saggiorato <david@saggiorato.net>
2015-03-11 10:55:20 +0000

    fix leak
    git-svn-id: http://svn.zoo-project.org/svn/branches/PublicaMundi_David-devel@606 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- zoo-project/zoo-kernel/zoo_json.h
+++ zoo-project/zoo-kernel/zoo_json.h
@@ -10,6 +10,6 @@ void maptojson(json_object ** obj,map * m);
 void mapstojson(json_object ** obj,maps * m);
 
 map* jsontomap(json_object * jobj);
-int jsontomaps(json_object * jobj,maps **m);
+maps*  jsontomaps(json_object * jobj);
 
 #endif
