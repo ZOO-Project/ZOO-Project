c8704c796b326f984a75b80a7821ce18cbf44711
David Saggiorato <david@saggiorato.net>
2015-03-11 10:55:20 +0000

    fix leak
    git-svn-id: http://svn.zoo-project.org/svn/branches/PublicaMundi_David-devel@606 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- zoo-project/zoo-kernel/zoo_json.c
+++ zoo-project/zoo-kernel/zoo_json.c
@@ -32,12 +32,25 @@ void mapstojson(json_object ** obj,maps * m){
 
 
 map* jsontomap(json_object * jobj){
-    map *m;
+    map *m = (map *) malloc (MAP_SIZE);
+    int i = 0;
     json_object_object_foreach(jobj, key, val){
         if (json_object_is_type(val, json_type_string)){
-            addMapToMap(&m,createMap(key,json_object_get_string(val)));
+            if (i==0){
+                m->name = zStrdup(key);
+                m->value = zStrdup(json_object_get_string(val));
+                m->next = NULL;
+                i = 1;
+            }
+            else {
+                map *tmp=createMap(key,json_object_get_string(val));
+                addMapToMap(&m,tmp);
+                freeMap(&tmp);
+                free(tmp);
+           }
         }
         else {
+            fprintf(stderr,"jsontomap not map :%s \n",key);
             /* Attention ce n'est pas une map */
             return NULL;
         }
@@ -45,15 +58,29 @@ map* jsontomap(json_object * jobj){
         return m;
 }
 
-int jsontomaps(json_object * jobj,maps ** m){
-    maps *maps_tmp;
+maps * jsontomaps(json_object * jobj){
+    maps *mm = (maps *) malloc (MAPS_SIZE); 
+    int i =0;
     json_object_object_foreach(jobj, key, val){
-        maps_tmp = (maps *) malloc (MAPS_SIZE);
-        maps_tmp->content = jsontomap(val); 
-        maps_tmp->name = zStrdup(key);
-        addMapsToMaps(m,maps_tmp);
+        if (i ==0){
+            mm->content = jsontomap(val);
+            mm->name = zStrdup(key);
+            mm->next = NULL;
+            i = 1;
+        }
+        else {
+            maps *maps_tmp;
+            maps_tmp = (maps *) malloc (MAPS_SIZE);
+            maps_tmp->content = jsontomap(val); 
+            maps_tmp->name = zStrdup(key);
+            maps_tmp->next = NULL;
+            addMapsToMaps(&mm,maps_tmp);
+            freeMaps(&maps_tmp);
+            free(maps_tmp);
+            maps_tmp = NULL;
+       }
     }
-    return 0;
+    return mm;
 }
  
 
