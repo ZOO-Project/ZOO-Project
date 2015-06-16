6de3fee9fb1c54ecad433c01a95df125c3bbfd5b
Nicolas Bozon <nicolas.bozon@gmail.com>
2015-06-16 14:52:46 +0000

    doc conf correction again
    git-svn-id: http://svn.zoo-project.org/svn/branches/PublicaMundi_David-devel@670 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ html_theme_path = ['_themes']
 modindex_common_prefix = ['sphinx.']
 html_static_path = ['_static']
 html_sidebars = {'index': ['indexsidebar.html', 'searchbox.html']}
-html_additional_pages = {'index': 'index.html'}
+#html_additional_pages = {'index': 'index.html'}
 html_use_opensearch = 'http://sphinx-doc.org'
 htmlhelp_basename = 'Sphinxdoc'
 
