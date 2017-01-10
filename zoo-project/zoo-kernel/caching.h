7ffeeac473ab372ffe2bd878c2bb9b7136dcccd9
Gérald Fenoy <gerald.fenoy@geolabs.fr>
2017-01-10 16:21:45 +0000

    Return an ExceptionReport in case any inpts failed to be downloaded.
    git-svn-id: http://svn.zoo-project.org/svn/trunk@797 4b6b6ad6-68b3-4dcc-9231-bd9b83bc5dc3

--- zoo-project/zoo-kernel/caching.h
+++ zoo-project/zoo-kernel/caching.h
@@ -31,7 +31,7 @@ extern "C" {
 
   void addToCache(maps*,char*,char*,char*,int,char*,size_t);
   char* isInCache(maps*,char*);
-  int runHttpRequests(maps**,maps**,HINTERNET*);
+  int runHttpRequests(maps**,maps**,HINTERNET*,map**);
   void addRequestToQueue(maps**,HINTERNET*,const char*,bool);
   int loadRemoteFile(maps**,map**,HINTERNET*,char*);
 
