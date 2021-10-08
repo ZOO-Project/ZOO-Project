8849ccd7196c1027537779ec3f328c01870841f2
Gérald FENOY <gfenoy@users.noreply.github.com>
2021-10-08 14:21:08 +0200

    Small change to support conversion of the b input
    Small change to support conversion of the b input from GeoJSON to GML and reverse.

--- zoo-project/zoo-services/echo-py/cgi-env/echo_service.py
+++ zoo-project/zoo-services/echo-py/cgi-env/echo_service.py
@@ -1,5 +1,7 @@
 import zoo
 import sys
+import osgeo.ogr
+import osgeo.ogr as ogr
 
 def echo(conf,inputs,outputs):
     if "a" in inputs:
@@ -13,7 +15,18 @@ def echo(conf,inputs,outputs):
     else:
         outputs["a"]["value"]="Empty"
     if "b" in inputs:
-        outputs["b"]["value"]=inputs["b"]["value"]
+        if "cache_file" in inputs["b"]:
+            ds = osgeo.ogr.Open(inputs["b"]["cache_file"])
+            path=""
+            if outputs["b"]["mimeType"]=="application/json":
+                path=conf["main"]["tmpPath"]+"/result-"+conf["lenv"]["usid"]+".json"
+                out_ds  = ogr.GetDriverByName("GeoJSON").CopyDataSource(ds, conf["main"]["tmpPath"]+"/result-"+conf["lenv"]["usid"]+".json")
+            else:
+                path=conf["main"]["tmpPath"]+"/result-"+conf["lenv"]["usid"]+".xml"
+                out_ds  = ogr.GetDriverByName("GML").CopyDataSource(ds, conf["main"]["tmpPath"]+"/result-"+conf["lenv"]["usid"]+".xml")
+        #outputs["b"]["value"]=open(path,"r",encoding="utf-8").read()
+        outputs["b"]["generated_file"]=path
+        #print(outputs["b"]["value"][:10],file=sys.stderr)
         if "dataType" in inputs["b"]:
             outputs["b"]["dataType"]=inputs["b"]["dataType"]
     else:
