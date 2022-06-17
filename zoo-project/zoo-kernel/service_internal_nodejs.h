b16c9ce7131e9dd65fb21405a62c2b37698e9db2
Momtchil Momtchev <momtchil@momtchev.com>
2022-06-17 10:45:41 +0200

    update the copyright notice

--- zoo-project/zoo-kernel/service_internal_nodejs.h
+++ zoo-project/zoo-kernel/service_internal_nodejs.h
@@ -1,7 +1,7 @@
 /*
  * Author : Momtchil MOMTCHEV
  *
- * Copyright (c) 2022
+ * Copyright 2022 Google (as part of GSoC 2022). All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
