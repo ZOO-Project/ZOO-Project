1ba0f3f9e9f36941f95728b73cd91d3f7f93413f
sdhoundiyal <sdhoundiyal@iitb.ac.in>
2022-06-25 15:31:39 +0530

    added three GCPs to the trail file intitalized randomly and the Coordinate Refrence System

--- zoo-project/zoo-services/qgis/trial.cpp
+++ zoo-project/zoo-services/qgis/trial.cpp
@@ -2,33 +2,74 @@
 #include "qgsgcppoint.h"
 #include "qgspointxy.h"
 #include "qgscoordinatereferencesystem.h"
-//#include "qgsgcptransformer.h"
+#include "qgsgcptransformer.h"
 //#inlcude "qgsgcpgeometrytransformer.h"
 
 int main(){
 
+	//specify the source coordinates for 3 GCPs
+	// format (x1,y1), (x2,y2), (x3,y3)
+	sourceCoordinates=[[30,41],[45,51],[60,21]]
+	//specify the destination coordinates for 3 GCPs
+	// format (x1,y1), (x2,y2), (x3,y3)
+	destinationCoordinates=[[43.6789,21.4567],[45.3421,23.4678],[47.7645,17.3456]]
+	//specify the destination Coordinate Reference System (CRS)
+	crsCode=4326 
+	destinationCRS=QgsCoordinateReferenceSystem.fromEpsgId(crsCode)
+
+
+
 	//create first GCP
- 
-	//specify the source coordinates of a ground control point (GCP)
-	//random values chosen
-	sourceX=30
-	sourceY=41
+ 	
+ 	//specify point and coordiante indices
+ 	pointIndex=0
+ 	xIndex=0
+ 	yIndex=1
 	//create a QGSPoint in the source coordinate system i.e. row no. and col no.
-	sourcePoint=QgsPointXY(sourceX,sourceY)
-	
-	//specify the destination coordinates of the ground control point
-	//random values chosen
-	destinationX=43.6789
-	destinationY=21.4567
+	sourcePoint=QgsPointXY(sourceCoordinates[pointIndex,xIndex],sourceCoordinates[pointIndex,yIndex])
 	//create a QGSPoint in the destination coordinate system
-	destinationPoint=QgsPointXY(destinationX,destinationY)
-
-	//specify the destination Coordinate Reference System (CRS) 
-	destinationCRS=QgsCoordinateReferenceSystem.fromEpsgId()
-
+	destinationPoint=QgsPointXY(destinationCoordinates[pointIndex,xIndex],destinationCoordinates[pointIndex,yIndex])
 	//create a CGP out of the source coordiantes, destination coordinates and the CRS and enable it
 	gcp1=QgsGcpPoint(sourcePoint,destinationPoint,destinationCRS,true)
 
-	std::cout << "Hello World \n";
+
+
+	//create second GCP
+ 
+	//specify point and coordiante indices
+ 	pointIndex=1
+
+	//create a QGSPoint in the source coordinate system i.e. row no. and col no.
+	sourcePoint=QgsPointXY(sourceCoordinates[pointIndex,xIndex],sourceCoordinates[pointIndex,xIndex])
+	//create a QGSPoint in the destination coordinate system
+	destinationPoint=QgsPointXY(destinationCoordinates[pointIndex,xIndex],destinationCoordinates[pointIndex,xIndex])
+	//create a CGP out of the source coordiantes, destination coordinates and the CRS and enable it
+	gcp3=QgsGcpPoint(sourcePoint,destinationPoint,destinationCRS,true)
+
+
+
+	//create third GCP
+ 
+	//specify point and coordiante indices
+ 	pointIndex=2
+
+	//create a QGSPoint in the source coordinate system i.e. row no. and col no.
+	sourcePoint=QgsPointXY(sourceCoordinates[pointIndex,xIndex],sourceCoordinates[pointIndex,xIndex])
+	//create a QGSPoint in the destination coordinate system
+	destinationPoint=QgsPointXY(destinationCoordinates[pointIndex,xIndex],destinationCoordinates[pointIndex,xIndex])
+	//create a CGP out of the source coordiantes, destination coordinates and the CRS and enable it
+	gcp3=QgsGcpPoint(sourcePoint,destinationPoint,destinationCRS,true)
+
+	
+	//create a GCP transformer which converts points from the source coordinate system to the destination coordinate system
+	
+
+
+	std::cout << "First GCP created \n";
+	
+
+
+
+
 	return 0;
 }
