9f576e207f077d92aeb5a7790e2bc570547dd82d
sdhoundiyal <sdhoundiyal@iitb.ac.in>
2022-07-10 21:27:33 +0530

    .

--- zoo-project/zoo-services/qgis/setting_up_python/setting_up.py
+++ zoo-project/zoo-services/qgis/setting_up_python/setting_up.py
@@ -1,39 +1,35 @@
 import sys
 import pandas as pd
-paths = sys.path
+
+paths=sys.path
 df=pd.DataFrame({'paths':paths})
 df.to_csv('./qgis_sys_paths.csv',index=False)
 
-
-
 import os
 import json
-env = dict(os.environ)
-rem = ['SECURITYSESSIONID', 'LaunchInstanceID', 'TMPDIR']
-_ = [env.pop(r,None) for r in rem]
+
+env=dict(os.environ)
+rem=['SECURITYSESSIONID', 'LaunchInstanceID', 'TMPDIR']
+_=[env.pop(r,None) for r in rem]
 
 
 with open('./qgis_env.json', 'w') as f:
     json.dump(env,f,ensure_ascii=False,indent=4)
 
-
-
 from platform import python_version
 print(python_version())
 
+qspath='./qgis_sys_paths.csv'
 
-# set up system paths
-qspath='./qgis_sys_paths.csv' 
-# provide the path where you saved this file.
 paths=pd.read_csv(qspath).paths.tolist()
 sys.path+=paths
 
+qepath='./qgis_env.json'
+js=json.loads(open(qepath,'r').read())
+for k, v in js.items():
+    os.environ[k]=v
 
-
-# In special cases, we might also need to map the PROJ_LIB to handle the projections
-os.environ['PROJ_LIB'] = '/Applications/Qgis.app/Contents/Resources/proj'
-
-
+os.environ['PROJ_LIB']='C:/Program Files/QGIS 3.22.3/share/proj'
 
 import PyQt5.QtCore
 from osgeo import gdal
@@ -43,19 +39,19 @@ from qgis.core import (QgsApplication,
                        QgsProcessingRegistry)
 from qgis.analysis import QgsNativeAlgorithms
 
+feedback=QgsProcessingFeedback()
+QgsApplication.setPrefixPath(js['HOMEPAt'], True)
+qgs=QgsApplication([], False)
+qgs.initQgis()
 
-#add native algorithms from QGIS to the processing directory
-feedback = QgsProcessingFeedback()# initializing processing module
-QgsApplication.setPrefixPath(js['HOME'], True)
-qgs = QgsApplication([], False)
-qgs.initQgis() # use qgs.exitQgis() to exit the processing module at the end of the script.# initialize processing algorithms
 from processing.core.Processing import Processing
 Processing.initialize()
-import processingQgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
+import processing
 
-
-#print the list of available algorithms
 algs = dict()
 for alg in QgsApplication.processingRegistry().algorithms():
     algs[alg.displayName()] = alg.id()
 print(algs)
+
+
+processing.algorithmHelp("native:georeference")
