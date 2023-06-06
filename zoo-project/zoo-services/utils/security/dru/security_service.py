8dc8fa89814e440c28818b41615a029b219beafe
gfenoy <gerald.fenoy@geolabs.fr>
2023-06-06 17:42:32 +0200

    Move the sample code for handling the namespace in utils/security/dru
    Add the additional zcfg file, simply duplicated from jwt and basicAuth

--- zoo-project/zoo-services/utils/security/dru/security_service.py
+++ zoo-project/zoo-services/utils/security/dru/security_service.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #  Author:   Gérald Fenoy, gerald.fenoy@geolabs.fr
-#  Copyright (c) 2020, GeoLabs SARL. 
+#  Copyright (c) 2020-2023, GeoLabs SARL. 
 ############################################################################### 
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
@@ -21,14 +21,25 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 ################################################################################
-from Cheetah.Template import Template
-import configparser
 import zoo
 
-def display(conf,inputs,outputs):
-    config = configparser.ConfigParser()
-    config.read(conf["lenv"]["cwd"]+'/oas.cfg',"utf-8")
-    nameSpace = {'conf': conf,'inputs': inputs, 'outputs': outputs,"openapi": config}
-    t = Template(file=conf["main"]["templatesPath"]+"/index.html",searchList=nameSpace)
-    outputs["Result"]["value"]=t.__str__()
+def securityIn(conf,inputs,outputs):
+    import sys,os,shutil
+    print("securityIn!",file=sys.stderr)
+    rPath=conf["servicesNamespace"]["path"]+"/"
+    for i in conf["renv"]:
+        if i.count("SERVICES_NAMESPACE"):
+            rPath+=conf["renv"][i]
+            conf["auth_env"]={"user": conf["renv"][i]}
+            break
+    if not(os.path.isdir(rPath)):
+        os.mkdir(rPath)
+        rFiles=["DeployProcess.py","DeployProcess.zcfg","UndeployProcess.py","UndeployProcess.zcfg","service.py","security_service.zo","securityOut.zcfg","deploy_util.py","securityIn.zcfg"]
+        for i in range(len(rFiles)):
+            shutil.copyfile(conf["renv"]["CONTEXT_DOCUMENT_ROOT"]+"/"+rFiles[i],rPath+"/"+rFiles[i])
+    return zoo.SERVICE_SUCCEEDED
+
+def securityOut(conf,inputs,outputs):
+    import sys
+    print("securityOut!",file=sys.stderr)
     return zoo.SERVICE_SUCCEEDED
